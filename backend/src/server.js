8aff0b63ea9ea076cf92a3909aab60792f27c12b
mounika <mounikads4816@gmail.com>
2025-12-23 13:32:53 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
+if (ENV.NODE_ENV !== "production") {
   app.use(
     cors({
       origin: "http://localhost:5173",
@@ -15,18 +15,22 @@ if (process.env.NODE_ENV !== "production") {
   );
 }
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
-
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
-
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "success api" });
 });
+
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "This is the books endpoint" });
+});
+
+// Make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // Any other route that user visits other than /health and /books, display the react app(index.html)
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
